ff20a5c66b86b2574ba6a27477a4eabe8e748db9
Skeev_ <derkach94@gmail.com>
2018-05-20 21:54:57 +0300

    5th widget w/o scrollbar

--- js/common.js
+++ js/common.js
@@ -14,4 +14,22 @@ $(function () {
             });
         }
     });
+    $(".owl-carousel").owlCarousel({
+        loop:false,
+        margin:10,
+        nav: true,
+        navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+        dots: false,
+        responsive:{
+            0:{
+                items:1,
+            },
+            640:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+        }
+    });
 });
